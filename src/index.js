7943e30d0486bef6967095299836edc5f80c6ee9
Joshua Rippon <joshuarippon1@gmail.com>
2018-10-14 22:26:59 +1300

    Add comment challenge part 3

--- src/index.js
+++ src/index.js
@@ -49,22 +49,26 @@ const comments = [
     {
         id: "101",
         text: "That sucks...",
-        author: "1"
+        author: "1",
+        post: "1"
     },
     {
         id: "102",
         text: "No it doesn't!",
-        author: "2"
+        author: "2",
+        post: "1"
     },
     {
         id: "103",
         text: "Yes it does.",
-        author: "1"
+        author: "1",
+        post: "1"
     },
     {
         id: "104",
         text: "No you are lying",
-        author: "3"
+        author: "3",
+        post: "1"
     }
 ];
 
@@ -92,12 +96,14 @@ const typeDefs = `
         body: String!
         author: User!
         published: Boolean
+        comments: [Comment]!
     }
     
     type Comment {
         id: ID!
         text: String!
         author: User!
+        post: Post!
     }
 `;
 
@@ -142,6 +148,9 @@ const resolvers = {
     Post: {
         author: (parent, args, ctx, info) => {
             return users.find(user => user.id === parent.author);
+        },
+        comments: (parent, args, ctx, info) => {
+            return comments.filter(comment => comment.post === parent.id);
         }
     },
     User: {
@@ -159,6 +168,9 @@ const resolvers = {
     Comment: {
         author: (parent, args, ctx, info) => {
             return users.find(user => user.id === parent.author);
+        },
+        post: (parent, args, ctx, info) => {
+            return posts.find(post => post.id === parent.post);
         }
     }
 };
